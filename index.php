c07463744e270089cd59c669382dbba2e2398704
pt_adv <a@htc.com>
2017-05-21 04:00:16 +0800

    commit index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+  require_once('../header.php');
+  require_once('flag.php');
+
+  if ($_GET["password"]=="9527"){
+
+    echo '<p>flag is ' . $flag . '</p>';
+  };
+  
+?>
+<p> You should know how to find the flag </p>
